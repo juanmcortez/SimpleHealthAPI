e79ffb6e4e56242ef8a093ff1d920ec47950904e
Juan M. Cortéz <juanm.cortez@gmail.com>
2023-03-06 16:00:44 -0300

    🚧 Persona model created

---
+++ app/Http/Controllers/V1/Common/PersonaController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers\V1\Common\Persona;
+
+use App\Models\V1\Common\Persona;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StorePersonaRequest;
+use App\Http\Requests\UpdatePersonaRequest;
+
+class PersonaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StorePersonaRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Persona $persona)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Persona $persona)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatePersonaRequest $request, Persona $persona)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Persona $persona)
+    {
+        //
+    }
+}
---
+++ database/factories/V1/Common/PersonaFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories\V1\Common;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\V1\Common\Persona>
+ */
+class PersonaFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
