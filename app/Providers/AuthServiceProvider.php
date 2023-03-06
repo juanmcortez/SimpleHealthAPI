34983d37c0676dc32815390355074b215a5b7ab3
Juan M. Cortéz <juanm.cortez@gmail.com>
2023-03-06 13:13:34 -0300

    🚧 Patient model policy created

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -14,6 +14,7 @@ class AuthServiceProvider extends ServiceProvider
      */
     protected $policies = [
         // 'App\Models\Model' => 'App\Policies\ModelPolicy',
+        'App\Models\V1\Patients\Patient'                => 'App\Policies\V1\Patients\PatientPolicy',
     ];
 
     /**
