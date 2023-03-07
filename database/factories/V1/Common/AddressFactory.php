6c67043e1202d7a40e2ee92b5006b3411a973eb3
Juan M. Cortéz <juanm.cortez@gmail.com>
2023-03-07 10:56:38 -0300

    🚧 Address Model created

---
+++ database/factories/V1/Common/AddressFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories\V1\Common;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\V1\Common\Address>
+ */
+class AddressFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'street_name'           => fake()->streetName(),
+            'street_name_extended'  => fake()->streetSuffix(),
+            'city'                  => fake()->city(),
+            'district'              => null,
+            'state'                 => fake()->citySuffix(),
+            'postal_code'           => fake()->postcode(),
+            'country_code'          => fake()->countryCode(),
+        ];
+    }
+}
