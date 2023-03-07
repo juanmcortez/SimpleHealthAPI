5b65bbd09d1e3f6af31202853761254aef1a46e5
Juan M. Cortéz <juanm.cortez@gmail.com>
2023-03-07 13:33:36 -0300

    🚧 Phone model resource created

---
+++ app/Http/Resources/V1/Common/PhoneResource.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Resources\V1\Common;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PhoneResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'Number'     => $this->full_phone,
+        ];
+    }
+}
